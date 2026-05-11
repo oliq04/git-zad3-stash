244b1df11bb0fa152972c7de21d818c80af76ef0
Oliwier Zyskowski <oliwierzyskowski04@gmail.com>
2026-05-11 16:18:05 +0200

    user profile pesel and age implementation

--- UserProfile.java
+++ UserProfile.java
@@ -4,14 +4,44 @@ public class UserProfile {
     private String name;
     private String email;
     private int age;
+    private int pesel;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPesel(int pesel) {
+        this.pesel = pesel;
+    }
+
+    public int getPesel() {
+        return pesel;
+    }
 
     public UserProfile(String name, String email, int age) {
         this.name = name;
         this.email = email;
         this.age = age;
+        this.pesel = pesel;
     }
 
-    public String getName() { return name; }
-    public String getEmail() { return email; }
-    public int getAge() { return age; }
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
