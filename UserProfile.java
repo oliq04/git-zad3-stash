d9ec2a284cc21c26368613b88b41d3401c28baef
Oliwier Zyskowski <oliwierzyskowski04@gmail.com>
2026-05-11 15:57:18 +0200

    feat: add UserProfile class

---
+++ UserProfile.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class UserProfile {
+    private String name;
+    private String email;
+
+    public UserProfile(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
