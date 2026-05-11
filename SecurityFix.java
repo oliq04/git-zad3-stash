7d4a3954d460d2cd3609fa27e3439832e453a924
Oliwier Zyskowski <oliwierzyskowski04@gmail.com>
2026-05-11 16:14:43 +0200

    fix: critical security patch intelij

--- SecurityFix.java
+++ SecurityFix.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class SecurityFix {
     public static void patch() {
-        System.out.println("Patched CVE-2025-1234");
+        System.out.println("Patched CVE-2025-18888234");
     }
 }
